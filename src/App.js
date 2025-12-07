d67dfad556f74e60abd1235e585c6999c9cacad4
Eduardo <eduffreitas@outlook.com>
2025-12-07 18:43:11 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
-import AboutPage from './pages/AboutPage';
-import AboutIconLink from './components/AboutIconLink';
-import { FeedbackProvider } from './context/FeedbackContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
   return (
     <FeedbackProvider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
-        <div className='container'>
+        <div className="container">
           <Routes>
             <Route
               exact
-              path='/'
+              path="/"
               element={
                 <>
                   <FeedbackForm />
@@ -26,7 +26,7 @@ function App() {
               }></Route>
 
             <Route
-              path='/about'
+              path="/about"
               element={<AboutPage />}></Route>
           </Routes>
 
